12543a2b44b3e8fd4ea512582bc24e3b1bf07902
Josh Long <josh@joshlong.com>
2018-07-29 21:22:19 -0700

    updated basic

--- basics/src/main/kotlin/com/example/basics/BasicsApplication.kt
+++ basics/src/main/kotlin/com/example/basics/BasicsApplication.kt
@@ -2,43 +2,42 @@ package com.example.basics
 
 import org.jetbrains.exposed.spring.SpringTransactionManager
 import org.jetbrains.exposed.sql.*
-import org.springframework.beans.factory.InitializingBean
+import org.springframework.beans.factory.SmartInitializingSingleton
 import org.springframework.boot.ApplicationRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.builder.SpringApplicationBuilder
-import org.springframework.context.support.beans
+import org.springframework.context.annotation.Bean
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import org.springframework.transaction.support.TransactionTemplate
+import javax.sql.DataSource
 
 @SpringBootApplication
-class BasicsApplication
+class BasicsApplication {
+
+
+	@Bean
+	fun springTransactionManager(ds: DataSource) = SpringTransactionManager(ds)
+
+	@Bean
+	fun applicationRunner(cs: CustomerService) = ApplicationRunner {
+		arrayOf("Josh", "Christian", "Markus", "Manjari",
+				"Billy", "Terri", "Ambili", "Chris")
+				.map { Customer(name = it) }
+				.forEach { cs.insert(it) }
+
+		cs.all().forEach {
+			println(cs.byId(it.id!!))
+		}
+	}
+
+
+}
 
 fun main(args: Array<String>) {
 
 	SpringApplicationBuilder()
 			.sources(BasicsApplication::class.java)
-			.initializers(beans {
-				bean {
-					SpringTransactionManager(ref())
-				}
-				bean {
-					ApplicationRunner {
-
-						val cs = ref<CustomerService>()
-
-						arrayOf("Josh", "Christian", "Markus", "Manjari",
-								"Billy", "Terri", "Ambili", "Chris")
-								.map { Customer(name = it) }
-								.forEach { cs.insert(it) }
-
-						cs.all().forEach {
-							println(cs.byId(it.id!!))
-						}
-
-					}
-				}
-			})
 			.run(*args)
 }
 
@@ -49,9 +48,9 @@ object Customers : Table() {
 
 @Service
 @Transactional
-class ExposedCustomerService(private val tt: TransactionTemplate) : CustomerService, InitializingBean {
+class ExposedCustomerService(private val tt: TransactionTemplate) : CustomerService, SmartInitializingSingleton {
 
-	override fun afterPropertiesSet() {
+	override fun afterSingletonsInstantiated() {
 		tt.execute {
 			SchemaUtils.create(Customers)
 		}
--- basics/src/test/kotlin/com/example/basics/BasicsApplicationTests.kt
+++ basics/src/test/kotlin/com/example/basics/BasicsApplicationTests.kt
@@ -9,6 +9,7 @@ import org.springframework.test.context.junit4.SpringRunner
 @SpringBootTest
 class BasicsApplicationTests {
 
+
 	@Test
 	fun contextLoads() {
 	}
