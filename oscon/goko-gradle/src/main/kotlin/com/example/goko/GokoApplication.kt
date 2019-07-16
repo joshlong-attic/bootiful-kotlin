3f61573116035c285391de243998c6d49f1cfc6c
Josh Long <josh@joshlong.com>
2019-07-16 19:15:00 -0400

    goko

---
+++ oscon/goko-gradle/src/main/kotlin/com/example/goko/GokoApplication.kt
@@ -0,0 +1,11 @@
+package com.example.goko
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GokoApplication
+
+fun main(args: Array<String>) {
+	runApplication<GokoApplication>(*args)
+}
