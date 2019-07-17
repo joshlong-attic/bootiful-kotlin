3984e43001c8eacabfdc092ba53b8999e8112d1f
Josh Long <josh@joshlong.com>
2019-07-16 18:53:58 -0600

    basics are working.

--- oscon/goko-maven/src/main/kotlin/com/example/goko/GokoApplication.kt
+++ oscon/goko-maven/src/main/kotlin/com/example/goko/GokoApplication.kt
@@ -1,13 +1,15 @@
 package com.example.goko
 
 import org.jetbrains.exposed.spring.SpringTransactionManager
-import org.jetbrains.exposed.sql.*
-import org.springframework.beans.factory.InitializingBean
+import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.sql.selectAll
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.event.ApplicationReadyEvent
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Profile
 import org.springframework.context.event.EventListener
+import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.stereotype.Component
 import org.springframework.stereotype.Service
 import org.springframework.transaction.PlatformTransactionManager
@@ -37,49 +39,14 @@ object Customers : Table() {
 	val name = varchar("name", 255)
 }
 
-@Component
-class SampleDataInitializer(private val customerService: CustomerService) {
-
-	@EventListener(ApplicationReadyEvent::class)
-	fun initialize() {
-
-		val names = arrayOf("Tammie", "Kimly", "Madhura", "Olga", "Cornelia", "Jennifer", "Michelle", "Eva")
-		names.map { Customer(name = it) }
-				.forEach { customerService.insert(it) }
-
-		customerService.all()
-				.forEach {
-					val id: Long = it.id!!
-					val customer = customerService.byId(id)
-					println(customer)
-				}
-	}
-}
-
+@Profile("exposed")
 @Service
 @Transactional
-class ExposedCustomerService(private val transactionTemplate: TransactionTemplate) : CustomerService, InitializingBean {
+class ExposedCustomerService : CustomerService {
 
-	override fun afterPropertiesSet() {
-		this.transactionTemplate.execute {
-			SchemaUtils.create(Customers)
-		}
-	}
-
-	override fun byId(id: Long): Customer? =
-			Customers
-					.select { Customers.id.eq(id) }
-					.map { Customer(id = it[Customers.id], name = it[Customers.name]) }
-					.singleOrNull()
-
-	override fun insert(customer: Customer) {
-		Customers.insert { it[Customers.name] = customer.name }
-	}
-
-	override fun all(): Collection<Customer> =
-			Customers
-					.selectAll()
-					.map { Customer(id = it[Customers.id], name = it[Customers.name]) }
+	override fun all(): Collection<Customer> = Customers
+			.selectAll()
+			.map { Customer(id = it[Customers.id], name = it[Customers.name]) }
 }
 
 
@@ -91,35 +58,18 @@ class CustomerRestController(private val customerService: CustomerService) {
 
 }
 
-/*
+@Profile("jdbc")
 @Service
 class JdbcCustomerService(private val jdbcTemplate: JdbcTemplate) : CustomerService {
 
-	private fun rsToCustomer(rs: ResultSet) = Customer(id = rs.getLong("ID"), name = rs.getString("NAME"))
-
-	override fun all(): Collection<Customer> =
-			this.jdbcTemplate.query("select * from CUSTOMERS") { rs, i ->
-				rsToCustomer(rs)
-			}
-
-	override fun byId(id: Long): Customer? =
-			this.jdbcTemplate.queryForObject("select * from CUSTOMERS where ID =?", id) { rs, i ->
-				rsToCustomer(rs)
-			}
-
-	override fun insert(customer: Customer) {
-		this.jdbcTemplate.execute("insert into CUSTOMERS(NAME) values(?)") {
-			it.setString(1, customer.name)
-			it.execute()
-		}
+	override fun all(): Collection<Customer> = this.jdbcTemplate.query("select * from CUSTOMERS") { rs, _ ->
+		Customer(id = rs.getLong("ID"), name = rs.getString("NAME"))
 	}
 }
-*/
+
 
 interface CustomerService {
 	fun all(): Collection<Customer>
-	fun byId(id: Long): Customer?
-	fun insert(customer: Customer)
 }
 
 data class Customer(val id: Long? = null, val name: String)
