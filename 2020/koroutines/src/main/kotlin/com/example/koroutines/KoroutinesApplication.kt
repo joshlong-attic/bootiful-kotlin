70bed0755d34ff7ecf24d2094131773122c35356
Josh Long <josh@joshlong.com>
2020-09-14 14:35:49 -0700

    koroutines

--- 2020/koroutines/src/main/kotlin/com/example/koroutines/KoroutinesApplication.kt
+++ 2020/koroutines/src/main/kotlin/com/example/koroutines/KoroutinesApplication.kt
@@ -6,21 +6,21 @@ import org.springframework.context.annotation.Bean
 import org.springframework.data.annotation.Id
 import org.springframework.data.r2dbc.core.DatabaseClient
 import org.springframework.data.r2dbc.core.asType
-import org.springframework.data.r2dbc.core.awaitOneOrNull
-import org.springframework.data.r2dbc.core.flow
+import org.springframework.data.r2dbc.core.awaitOne
 import org.springframework.stereotype.Repository
 import org.springframework.web.reactive.function.server.ServerResponse
-import org.springframework.web.reactive.function.server.bodyAndAwait
+import org.springframework.web.reactive.function.server.bodyValueAndAwait
 import org.springframework.web.reactive.function.server.coRouter
 
 @SpringBootApplication
 class KoroutinesApplication {
 
-  @Bean
+  @Bean()
   fun routes(cr: CustomerRepository) =
       coRouter {
-        GET("/customers") {
-          ServerResponse.ok().bodyAndAwait(cr.all())
+        GET("/customers/{id}") {
+          val customer: Customer = cr.findOne(it.pathVariable("id"))
+          ServerResponse.ok().bodyValueAndAwait(customer)
         }
       }
 }
@@ -29,30 +29,19 @@ fun main(args: Array<String>) {
   runApplication<KoroutinesApplication>(*args)
 }
 
-
-data class Customer(
-    @Id val id: Int,
-    val name: String
-)
-
+data class Customer(@Id val id: Int, val name: String)
 
 @Repository
 class CustomerRepository(private val dbc: DatabaseClient) {
 
-  suspend fun all() =
-      this.dbc.select()
-          .from("CUSTOMER")
-          .asType<Customer>()
-          .fetch()
-          .flow()
 
-  suspend fun findOne(name: String): Customer? =
+  suspend fun findOne(name: String): Customer =
       this.dbc
           .execute("select * from CUSTOMER where NAME = :name")
           .bind("name", name)
           .asType<Customer>()
           .fetch()
-          .awaitOneOrNull()
+          .awaitOne()
 
 
 }
