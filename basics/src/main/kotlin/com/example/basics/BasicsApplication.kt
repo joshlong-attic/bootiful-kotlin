62f846738065b7ff7b2fbdee7236ee316190e035
Josh Long <josh@joshlong.com>
2021-02-18 23:17:07 -0800

    Update BasicsApplication.kt

--- basics/src/main/kotlin/com/example/basics/BasicsApplication.kt
+++ basics/src/main/kotlin/com/example/basics/BasicsApplication.kt
@@ -15,42 +15,42 @@ import org.springframework.transaction.annotation.Transactional
 @SpringBootApplication
 class BasicsApplication {
 
-  @Bean
-  fun runner(cs: CustomerService) = ApplicationListener<ApplicationReadyEvent> {
-    cs.all().forEach { println(it) }
-  }
+	@Bean
+	fun runner(cs: CustomerService) = ApplicationListener<ApplicationReadyEvent> {
+		cs.all().forEach { println(it) }
+	}
 }
 
 fun main(args: Array<String>) {
-  runApplication<BasicsApplication>(*args)
+	runApplication<BasicsApplication>(*args)
 }
 
-object Customers : Table() {
-  val id = integer("id").autoIncrement()
-  val name = varchar("name", 255)
+object Customers : Table("customer") {
+	val id = integer("id").autoIncrement()
+	val name = varchar("name", 255)
 }
 
 @Service
 @Transactional
 class ExposedCustomerService : CustomerService {
-  
-  override fun all() =
-    Customers.selectAll().map { Customer(it[Customers.id], it[Customers.name]) }
+
+	override fun all() =
+		Customers.selectAll().map { Customer(it[Customers.id], it[Customers.name]) }
 }
 
 @Service
 @Profile("jdbc")
 class JdbcCustomerService(val jdbcTemplate: JdbcTemplate) : CustomerService {
 
-  override fun all() =
-      this.jdbcTemplate.query("select * from CUSTOMER") { rs, index ->
-        Customer(rs.getInt("id"), rs.getString("name"))
-      }
+	override fun all() =
+		this.jdbcTemplate.query("select * from CUSTOMER") { rs, index ->
+			Customer(rs.getInt("id"), rs.getString("name"))
+		}
 }
 
 data class Customer(val id: Int, val name: String)
 
 interface CustomerService {
 
-  fun all(): Collection<Customer>
+	fun all(): Collection<Customer>
 }
